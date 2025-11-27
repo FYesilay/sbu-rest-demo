69c3503e322bae4fa3244c1144799f862318a84f
FYesilay <farukyesilay@outlook.de>
2025-11-27 21:10:09 +0100

    @Value application.properties

--- src/main/java/com/thechecklers/sbu_rest_demo/SbuRestDemoApplication.java
+++ src/main/java/com/thechecklers/sbu_rest_demo/SbuRestDemoApplication.java
@@ -1,12 +1,15 @@
 package com.thechecklers.sbu_rest_demo;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -112,5 +115,39 @@ class RestApiDemoController {
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
 		coffeeRepository.deleteById(id);
+	}
+}
+
+@Component
+class DataLoader {
+	private final CoffeeRepository coffeeRepository;
+	public DataLoader(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+	}
+	@PostConstruct
+	private void loadData() {
+		coffeeRepository.saveAll(List.of(
+				new Coffee("Café Cereza"),
+				new Coffee("Café Ganador"),
+				new Coffee("Café Lareño"),
+				new Coffee("Café Três Pontas")
+		));
+	}
+}
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController{
+	@Value("${greeting-name: Mirage}")
+	private String name;
+	@Value("${greeting-coffee: ${greeting-name} is drinking Cafe Ganador}")
+	private String coffee;
+	@GetMapping
+	String getGreeting(){
+		return name;
+	}
+	@GetMapping("/coffee")
+	String getNameAndCoffee(){
+		return coffee;
 	}
 }
